9cac78b2daa0489d4c4504553a35f5f0423bb51a
anna-sergienko <85996689+anna-sergienko@users.noreply.github.com>
2023-07-13 15:11:33 +0000

    Deploying to gh-pages from @ anna-sergienko/goit-react-hw-08-phonebook@e21ea7334aa5cd6569ed3a4c34245f7741642d6d 🚀

--- static/js/415.2dcd7d6d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(e,n,o){o.r(n),o.d(n,{default:function(){return t}});var r=o(184);function t(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"Wellcome to our service!"})})}}}]);
-//# sourceMappingURL=415.2dcd7d6d.chunk.js.map
